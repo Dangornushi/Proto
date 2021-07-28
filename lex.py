6baa32e321bb3de83b23dbf4287ccb73a43c7494
dangomushi <>
2021-07-28 12:23:23 +0900

    init

---
+++ lex.py
@@ -0,0 +1,109 @@
+import ply.lex as lex
+
+tokens = (
+#    "STR",
+    "NUMBER",
+    "ID",
+    "STR",
+    "IF",
+    "ELSE",
+    "ELIF",
+    "WHILE",
+    "PLUS",
+    "MINUS",
+    "KAKERU",
+    "WARU",
+    "EQOL",
+    "EQOLS",
+    "DAINARI",
+    "SYOUNARI",
+    "CONMA",
+    "PIRIOD",
+    "KAKKO",
+    "KOKKA",
+    "LKAKKO",
+    "LKOKKA",
+    "TYPE",
+    "PUT",
+    "COLON",
+    "SEMI",
+    "END",
+    "QOT",
+    "FN",
+    "RETURN",
+    "INPUT",
+    "OR",
+    "AND",
+)
+
+t_CONMA = r","
+t_PIRIOD = r"\."
+t_PLUS = r"\+"
+t_MINUS = r"\-"
+t_KAKERU = r"\*"
+t_WARU = r"\/"
+t_EQOL = r"\="
+t_EQOLS = r"\=\="
+t_KAKKO = r"\("
+t_KOKKA = r"\)"
+t_NUMBER = r"\d+"
+t_LKAKKO = r"\["
+t_LKOKKA = r"\]"
+t_ignore = r' \t'
+t_COLON = r"\:"
+t_SEMI = r"\;"
+t_DAINARI = r"\>"
+t_SYOUNARI = r"\<"
+t_QOT = r"\""
+t_OR = r"\|\|"
+t_AND = "&&"
+
+def t_STR (t):
+    r"[\"'][_\<\>\.,\*+-/!?a-zA-Z0-9\"' ]*"
+    return t
+
+def t_ID (t):
+    r"[a-zA-Z_][a-zA-Z0-9_|\&]*"
+    if t.value == "int":
+        t.type = "TYPE"
+    elif t.value == "str":#t.value == "float" or 
+        t.type = "TYPE"
+    elif t.value == "void":
+        t.type = "TYPE"
+    elif t.value == "put":
+        t.type = "PUT"
+    elif t.value == "end":
+        t.type = "END"
+    elif t.value == "if":
+        t.type = "IF"
+    elif t.value == "else":
+        t.type = "ELSE"
+    elif t.value == "elif":
+        t.type = "ELIF"
+    elif t.value == "while":
+        t.type = "WHILE"
+    elif t.value == "fn":
+        t.type = "FN"
+    elif t.value == "return":
+        t.type = "RETURN"
+    elif t.value == ",":
+        t.type = "CONMA"
+    elif t.value == "input":
+        t.type = "INPUT"
+    elif t.value == "&&" or t.value == "and":
+        t.type = "AND"
+    elif t.value == "||" or t.value == "or":
+        t.type = "OR"
+    else:
+        t.type == "ID"
+    return t
+
+def t_error(t):
+    print("LexErr：%s, それ、あなたの感想ですよね？" % t.value[0])
+    t.lexer.skip(1)
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+lexer = lex.lex(debug=0)
---
+++ main.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+int opc = 0;
+int dc = 0;
+char stack[256][256] = {};
+char *mode;
+
+int main( int argc, char *argv[] ) {
+    FILE *file;
+    int data;
+    char stack[256][256];
+    char datas[256][256] = {};
+    char command[16] = "python3 main.py ";
+    char new_command[256];
+    sprintf(new_command, "%s%s", command, argv[1]);
+    system(new_command);
+    char file_name[256];
+    file = fopen(strtok(argv[1], "."), "rb");    // ファイルを読み込み用にオープン(開く)
+    while ( (data=fgetc(file)) != EOF){
+        switch (data) {
+            case 0x3b: {
+                char *line_data; //TODO : 一行ずつのデータ、ここから切り出して処理
+                char *opcode;
+                for ( int deel = 0; deel < sizeof(datas[dc]); deel++ ) {
+                    if ( datas[dc][deel] != 0x00 ) {
+                        line_data = &datas[dc][deel];
+                        switch (line_data[0]) {
+                            case 0x21: {
+                                //TODO : mov
+                                if ( strcmp(mode,"i") == 0) {
+                                    //TODO : もし代入の値が整数型であれば
+                                    int op1 = atoi(strtok(&line_data[2], "/"));// &;
+                                    int op2 = atoi(strtok(NULL, "/"));
+                                    stack[op1][0] = op2;                        
+                                }
+                                else
+                                if ( strcmp(mode,"r") == 0) {
+                                    int op1 = atoi(strtok(&line_data[2], "/"));
+                                    int op2 = atoi(&strtok(NULL, "/")[1]);
+                                    stack[op1][0] = stack[op2][0];
+                                }
+                                else {
+                                    int linesize = 0;
+                                    for (;;) {
+                                        if (line_data[linesize] != 0x00 ) { linesize++; }
+                                        else { break; }
+                                    }
+                                    int op1;
+                                    char *op2;
+                                    char *tp;
+                                    tp = strtok( line_data, "/" );
+                                    int size = 0;
+                                    op1 = atoi(&tp[2]);
+                                    int c = 0;
+                                    while ( 1 ) {
+                                        tp = strtok( NULL,"/" );
+                                        if ( tp != NULL ) {
+                                            op2 = tp;
+                                        }
+                                        else { 
+                                            break;
+                                        }
+                                        c++;
+                                    }
+                                    for ( size_t i = 0; i < linesize; i++ ) {
+                                        stack[op1][i] = op2[i];
+                                    }
+                                    
+                                }
+                                break;
+                            }
+
+                            case 0x22: {
+                                if (strcmp(mode,"s") == 0) {
+                                    printf("%s\n", stack[atoi(&line_data[1])]);
+                                }
+                                else {
+                                    printf("%d\n", stack[atoi(&line_data[1])][0]);
+                                }
+                                break;
+                            }
+
+                            case 0x2a: {
+                                char *base = strtok(line_data, "*");
+                                int op1 = atoi(strtok(base, "/"));
+                                int op2 = atoi(strtok(NULL, "/"));
+                                stack[op1][0] = stack[op1][0] * stack[op2][0];
+                                break;
+                            }
+
+                            case 0x2d: {
+                                char *base = strtok(line_data, "-");
+                                int op1 = atoi(strtok(base, "/"));
+                                int op2 = atoi(strtok(NULL, "/"));
+                                stack[op1][0] = stack[op1][0] - stack[op2][0];
+                                break;
+                            }
+
+                            case 0x2f: {
+                                char *base = strtok(line_data, "/");
+                                int op1 = atoi(strtok(base, "/"));
+                                int op2 = atoi(strtok(NULL, "/"));
+                                stack[op1][0] = stack[op1][0] / stack[op2][0];
+                                break;
+                            }
+
+                            case 0x2b: {
+                                char *base = strtok(line_data, "+");
+                                int op1 = atoi(strtok(base, "/"));
+                                int op2 = atoi(strtok(NULL, "/"));
+                                stack[op1][0] = stack[op1][0] + stack[op2][0];
+                                break;
+                            }
+
+                            case 0x3e: {
+                                char *now_mode = &line_data[1];
+                                mode = now_mode;
+                                break;
+                            }
+                            
+                            default:
+                                break;
+                        }
+                        
+                        break;
+                    }
+                }
+                
+                dc++;
+                break;
+            }
+            
+            default: {
+                if ( data != 0x00 ) {
+                    datas[dc][opc] = data;
+                    opc++;
+                }
+                break;
+            }
+        }
+        
+    }
+    fclose(file);
+}
